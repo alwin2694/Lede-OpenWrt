27d50314eca19221af31e4d0271a11e24c889c1b
281677160 <66651997+281677160@users.noreply.github.com>
2020-07-31 00:26:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,18 +10,23 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-#luci-theme-edge主题
-git clone https://github.com/garypang13/luci-theme-edge package/luci-theme-edge -b 18.06
-#luci-theme-edge主题颜色微调
-sed -i 's@background-color: #e5effd@background-color: #f8fbfe@g' package/luci-theme-edge/htdocs/luci-static/edge/cascade.css
-#luci-theme-edge主题颜色微调
-sed -i 's#rgba(223, 56, 18, 0.04)#rgba(223, 56, 18, 0.02)#g' package/luci-theme-edge/htdocs/luci-static/edge/cascade.css
+#取消掉feeds.conf.default文件里面的helloworld的#注释
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
+
+#设置密码为空
+sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
+
+
+#主题文件
+git clone https://github.com/garypang13/luci-theme-edge package/luci-theme-edge -b 18.06
+sed -i 's@background-color: #e5effd@background-color: #f8fbfe@g' package/luci-theme-edge/htdocs/luci-static/edge/cascade.css #luci-theme-edge主题颜色微调
+sed -i 's#rgba(223, 56, 18, 0.04)#rgba(223, 56, 18, 0.02)#g' package/luci-theme-edge/htdocs/luci-static/edge/cascade.css #luci-theme-edge主题颜色微调
 
 
 #添加自定义插件链接
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default #passwall出国软件
 sed -i '$a src-git smartdns https://github.com/pymumu/luci-app-smartdns.git' feeds.conf.default
 git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter #网页过滤
-git clone https://github.com/awesome-openwrt/luci-app-openclash package/luci-app-openclash #openclash出国软件
-git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan #微信推送信息通知
+#git clone https://github.com/awesome-openwrt/luci-app-openclash package/luci-app-openclash #openclash出国软件
+#git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan #微信推送信息通知
