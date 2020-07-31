80b63dbf36db07a93ae9cdc54681da7572266fb6
281677160 <66651997+281677160@users.noreply.github.com>
2020-07-31 13:12:57 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,22 +13,17 @@
 #取消掉feeds.conf.default文件里面的helloworld的#注释,使用ShadowSocksR Plus+出国软件
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
+sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings #设置密码为空
 
-#设置密码为空
-sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
-
-
-#主题文件
-git clone https://github.com/garypang13/luci-theme-edge package/luci-theme-edge -b 18.06
+git clone https://github.com/garypang13/luci-theme-edge package/luci-theme-edge -b 18.06 #主题
 sed -i 's@background-color: #e5effd@background-color: #f8fbfe@g' package/luci-theme-edge/htdocs/luci-static/edge/cascade.css #luci-theme-edge主题颜色微调
 sed -i 's#rgba(223, 56, 18, 0.04)#rgba(223, 56, 18, 0.02)#g' package/luci-theme-edge/htdocs/luci-static/edge/cascade.css #luci-theme-edge主题颜色微调
-
 
 #添加自定义插件链接
 git clone -b v1.x https://github.com/tano-systems/luci-app-tn-snmpd.git package/luci-app-tn-snmpd #snmpd
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default #passwall出国软件
 #git clone https://github.com/awesome-openwrt/luci-app-openclash package/luci-app-openclash #openclash出国软件
-git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash #clash出国软件
+git clone https://github.com/frainzy1477/luci-app-clash package/feeds/luci-app-clash #clash出国软件
 #git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan #微信推送信息通知
 git clone https://github.com/lariboo/luci-app-control-mia package/luci-app-control-mia #时间控制
 git clone https://github.com/lariboo/luci-app-control-webrestriction package/luci-app-control-webrestriction #访问控制
