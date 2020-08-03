72ddfcd35ad41681f20ce416d38f193d5d4340d6
281677160 <66651997+281677160@users.noreply.github.com>
2020-08-03 15:42:07 +0800

    Update diy-lede.sh

--- diy-lede.sh
+++ diy-lede.sh
@@ -30,17 +30,18 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luc
 
 
 #添加自定义插件链接
-#此链接带(passwall出国软件,时间控制luci-app-timecontrol,访问控制luci-app-control-weburl,网址过滤luci-app-control-weburl)
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#下面第一个链接带(passwall出国软件,时间控制luci-app-timecontrol,访问控制luci-app-control-weburl,网址过滤luci-app-control-weburl)
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default  #(passwall,时间控制,访问控制,网址过滤)
 
 git clone -b v1.x https://github.com/tano-systems/luci-app-tn-snmpd.git package/luci-app-tn-snmpd  #snmpd
 git clone https://github.com/awesome-openwrt/luci-app-openclash package/luci-app-openclash  #openclash出国软件
 #git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash  #clash出国软件
 #git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan  #微信推送信息通知
-#git clone https://github.com/lariboo/luci-app-control-mia package/luci-app-control-mia  #时间控制或者luci-app-timecontrol
-#git clone https://github.com/lariboo/luci-app-control-webrestriction package/luci-app-control-webrestriction  #访问控制
-#git clone https://github.com/lariboo/luci-app-control-weburl package/luci-app-control-weburl  #网址过滤
 git clone https://github.com/destan19/OpenAppFilter.git package/luci-app-oaf  #应用过滤
 #git clone https://github.com/s1oz/luci-app-koolproxyR.git package/luci-app-koolproxyR  #广告过滤
 #git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns  #smartdns DNS加速
 #git clone https://github.com/garypang13/luci-app-eqos package/luci-app-eqos  #IP地址限速
+
+#git clone https://github.com/lariboo/luci-app-control-mia package/luci-app-control-mia  #时间控制
+#git clone https://github.com/lariboo/luci-app-control-weburl package/luci-app-control-weburl  #网址过滤
+#git clone https://github.com/lariboo/luci-app-control-webrestriction package/luci-app-control-webrestriction  #访问控制
