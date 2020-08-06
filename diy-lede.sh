fc50f7df1058d26d12e93f1ad66e04677e674942
281677160 <66651997+281677160@users.noreply.github.com>
2020-08-06 11:50:14 +0800

    Update diy-lede.sh

--- diy-lede.sh
+++ diy-lede.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 cd openwrt
+rm -rf package/lean/luci-theme-argon
+rm -rf package/lean/luci-app-accesscontrol
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
 # 说明：
 # 除了第一行的#!/bin/bash不要动，其他的设置，前面带#表示不起作用，不带的表示起作用了（根据你自己需要打开或者关闭）
 #
-rm -rf package/lean/luci-theme-argon
 
 # 修改openwrt登陆地址,把下面的192.168.2.2修改成你想要的就可以了，其他的不要动
 sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
